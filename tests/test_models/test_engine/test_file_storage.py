939de73983c4ee3dd2c51530b4f957eae6cfe9fb
leuel-a <leuela1993@gmail.com>
2024-02-23 21:38:08 +0300

    Add unit tests for FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,53 +1,72 @@
 #!/usr/bin/python3
 """Defines the TestFileStorage class for unit testing the FileStorage class.
 """
+import json
+import os
 import unittest
 
+from models import storage
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """Test cases for the FileStorage class.
-
-    This class defines several test methods to ensure the correct behavior of
-    FileStorage functionalities, particularly the `all()` method.
+    """A class used to test the FileStorage class.
 
     Attributes:
-        mock_storage (FileStorage): A mock instance of FileStorage for testing.
-        prev_file_name (str): The original file path of FileStorage before
-                                testing.
+        prev_file_name (str): The previous file name of the FileStorage instance.
+        prev_objects (dict): A copy of the previous objects of the FileStorage instance.
     """
 
     def setUp(self) -> None:
-        """Sets up the test environment.
-
-        Creates a mock instance of FileStorage and temporarily changes the
-        _FileStorage__file_path attribute to "test.json" for testing purposes.
-        """
-        self.mock_storage = FileStorage()
-        self.prev_file_name = FileStorage._FileStorage__file_path
+        """Set up the test environment before each test."""
+        self.prev_file_name: str = FileStorage._FileStorage__file_path
+        self.prev_objects: dict = FileStorage._FileStorage__objects.copy()
+        FileStorage._FileStorage__objects = {}
         FileStorage._FileStorage__file_path = "test.json"
 
     def tearDown(self) -> None:
-        """Restores the original test environment.
-
-        Reverts the _FileStorage__file_path attribute back to original value.
-        """
+        """Clean up after each test."""
         FileStorage._FileStorage__file_path = self.prev_file_name
+        FileStorage._FileStorage__objects = self.prev_objects.copy()
+
+        if os.path.isfile("test.json"):
+            os.remove("test.json")
 
     def test_all(self) -> None:
-        """Tests the all() method of FileStorage.
+        """Test the all method of the FileStorage class."""
+        # Check if the dictionary is empty before any new object is added
+        self.assertDictEqual({}, storage.all())
 
-        Verifies that the `all()` method of the FileStorage class returns
-        the same dictionary as its internal `_FileStorage__objects` attribute.
+        # Check if a new object creation is added to the _FileStorage__objects
+        # dictionary
+        test_model = BaseModel()
+        key = f"BaseModel.{test_model.id}"
+        self.assertTrue(storage.all().get(key, None) is not None)
 
-        Raises:
-            AssertionError: If the dictionaries returned by `all()` and
-                `_FileStorage__objects` are not equal.
-        """
-        self.assertDictEqual(
-            self.mock_storage._FileStorage__objects, self.mock_storage.all()
-        )
+    def test_new_object_creation_added_to_the_storage(self) -> None:
+        """Test if a new object creation is added to the storage."""
+        new_object = BaseModel()
+
+        key = new_object.__class__.__name__ + "." + new_object.id
+        expected_dict = {key: new_object}
+        self.assertDictEqual(expected_dict, storage.all())
+
+    def test_reload_method(self):
+        """Test the reload method of the FileStorage class."""
+        # Add a new object from storage
+        storage.new(BaseModel())
+
+        # Store the objects to the storage
+        storage.save()
+        prev_objects = storage.all()
+
+        # Clear the _FileStorage__objects dictionary
+        storage.all().clear()
+
+        # Reload objects from the storage
+        storage.reload()
+        self.assertDictEqual(prev_objects, storage.all())
 
 
 if __name__ == "__main__":
